5e47d3ab644b46a0d507ffca1375c082dfc1d990
unknown <renatahassum@gmail.com>
2023-05-22 09:37:35 -0300

    User by id

--- backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
@@ -22,7 +22,7 @@ public class JwtTokenEnhancer implements TokenEnhancer {
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         User user = userRepository.findByEmail(authentication.getName());
         Map<String, Object> map = new HashMap<>();
-        map.put("userFirstName", user.getUsername());
+        map.put("userName", user.getUsername());
         map.put("userId", user.getId());
 
         DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
