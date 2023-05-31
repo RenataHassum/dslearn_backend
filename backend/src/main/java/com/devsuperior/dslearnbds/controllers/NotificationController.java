fe10bd4d10a4e173f95e6f0dd7147da4c78a6cc7
Renata Hassum <renatahassum@gmail.com>
2023-05-31 13:20:35 -0300

    Notification: all or unread

--- backend/src/main/java/com/devsuperior/dslearnbds/controllers/NotificationController.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/NotificationController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/notifications")
@@ -18,9 +15,17 @@ public class NotificationController {
     @Autowired
     private NotificationService service;
 
+//    @GetMapping
+//    public ResponseEntity<Page<NotificationDto>> notificationsForCurrentUser(Pageable pageable) {
+//        Page<NotificationDto> page = service.notificationsForCurrentUser(pageable);
+//        return ResponseEntity.ok().body(page);
+//    }
+
     @GetMapping
-    public ResponseEntity<Page<NotificationDto>> notificationsForCurrentUser(Pageable pageable) {
-        Page<NotificationDto> page = service.notificationsForCurrentUser(pageable);
+    public ResponseEntity<Page<NotificationDto>> notificationsForCurrentUser(
+            @RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable) {
+        Page<NotificationDto> page = service.notificationsForCurrentUser(unreadOnly, pageable);
         return ResponseEntity.ok().body(page);
     }
 }
